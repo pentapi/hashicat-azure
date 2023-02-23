42d4f8b525217a0d705dd325e21d7b7a78e3e30a
pentapi <pentapitester@gmail.com>
2023-02-23 00:22:31 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/org-hashicat-azure/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  azurerm_resource_group.myresourcegroup.id
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
